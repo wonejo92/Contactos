8b69d8e2fcfe5e5503e3706b28aa47ce0cbd2d42
wonejo.92 <ismael-3310@hotmail.com>
2021-06-17 11:50:29 -0500

    New features

--- contactos/src/app/pages/contactos/lista/lista.page.ts
+++ contactos/src/app/pages/contactos/lista/lista.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-lista',
@@ -8,7 +9,7 @@ import { NavigationExtras, Router } from '@angular/router';
 })
 export class ListaPage implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,public actionSheetController: ActionSheetController) { }
 
   ngOnInit() {
   }
@@ -32,6 +33,44 @@ export class ListaPage implements OnInit {
   showInfoContact(nombre:String,telefono:String,mail:String){
     console.log("El contacto seleccionado es" + " Nombre: " + nombre + " Telefono: " + telefono + " Mail: "+ mail)
   }
+  async showMenu(nombre:string) {
+    const actionSheet = await this.actionSheetController.create({
+      header: nombre,
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Call',
+        icon: 'call',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      },{
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
+
+}
 
 
