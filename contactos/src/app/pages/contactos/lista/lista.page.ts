1af6003e95a03e9aa360d32d50b62c6a5f5c57fc
wonejo.92 <ismael-3310@hotmail.com>
2021-06-16 23:43:15 -0500

    Nuevas Funcionalidades

--- contactos/src/app/pages/contactos/lista/lista.page.ts
+++ contactos/src/app/pages/contactos/lista/lista.page.ts
@@ -12,9 +12,21 @@ export class ListaPage implements OnInit {
 
   ngOnInit() {
   }
+
+  public contactos = [
+    { Nombre: 'Juan Andrade', Telefono: '0991463702', Mail: 'Jandrade@gmail.com' },
+    { Nombre: 'Pedro Calle', Telefono: '0993296123', Mail: 'Pcalle@gmail.com' },
+    { Nombre: 'Vanesa Leon', Telefono: '0993296364', Mail: 'Vleon@gmail.com' },
+    { Nombre: 'Maria Mena', Telefono: '0986411830', Mail: 'Mmena@gmail.com' },
+  ];
+
   add(){
     this.router.navigate(['crear'])
-  }   
+  }  
+  
+  showInfoContact(nombre:String,telefono:String,mail:String){
+    console.log("El contacto seleccionado es" + " Nombre: " + nombre + " Telefono: " + telefono + " Mail: "+ mail)
+  }
   }
 
 
